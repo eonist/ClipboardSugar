83bbfc15d7781e14cadc0307725a2d25117b66ab
Eonist <30n1st@gmail.com>
2019-12-05 19:46:34 +0100

    Add lint

--- ClipboardSugarExampleMacOS/AppDelegate.swift
+++ ClipboardSugarExampleMacOS/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
    }
 }
 
-open class View: NSView{
+open class View: NSView {
    override open var isFlipped: Bool { return true }/*TopLeft orientation*/
    override public init(frame: CGRect) {
       super.init(frame: frame)
@@ -29,7 +29,7 @@ open class View: NSView{
    /**
     * Boilerplate
     */
-   required public init?(coder decoder: NSCoder) {
+   public required init?(coder decoder: NSCoder) {
       fatalError("init(coder:) has not been implemented")
    }
 }
